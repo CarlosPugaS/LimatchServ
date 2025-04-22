54f2d677bc2c7e9f4437bba8e6bdb301aab1a82d
Carlos Puga Salinas <c.puga26@gmail.com>
2025-04-22 17:19:29 -0400

    Se agrega .gitignore y remueve .env del control de versiones

--- Back/app.py
+++ Back/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from config import config
+from config import Config
 
 db = SQLAlchemy()
 
 def create_app():
   app = Flask(__name__)
-  app.config.from_object(config)
+  app.config.from_object(Config)
   db.init_app(app)
   CORS(app)
 
@@ -18,4 +18,6 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
+    with app.app_context():
+      db.create_all();
     app.run(debug=True)
--- Back/config.py
+++ Back/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 # Cargar las variables desde el archivo .env
 load_dotenv()
 
-class config:
+class Config:
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
         f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
