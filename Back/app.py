0e285ee9a5e11b6a1b585130e4d2a7e9269dfebe
CarlosPugaS <c.puga26@gmail.com>
2025-04-25 18:51:55 -0400

    Creación de entidades e instalación de Flask-Migrate

--- Back/app.py
+++ Back/app.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from config import Config
-
-db = SQLAlchemy()
+from flask_migrate import Migrate
+from models.entities import db
 
 def create_app():
   app = Flask(__name__)
   app.config.from_object(Config)
   db.init_app(app)
+  migrate = Migrate(app, db)
   CORS(app)
 
   @app.route('/')
@@ -18,6 +19,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    with app.app_context():
-      db.create_all()
     app.run(debug=True)
